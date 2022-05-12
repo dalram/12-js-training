6be2e9991c8d6e78b4e081783d41059959e31902
Dalius Ram <ramonas.dalius@gmail.com>
2022-05-12 20:33:08 +0300

    update

--- 05-data/app.js
+++ 05-data/app.js
@@ -13,26 +13,52 @@ function decimalRand(min, max) {
 }
 
 const products = ['Dviratis', 'Triratis', 'Žvakidė', 'Stalas', 'Kėdė', 'Knygų lentyna', 'Paveikslas su rėmu', 'Veidrodis', 'Keturių vietų palapinė', 'Pripučiama valtis', 'Meškerė karosams gaudyti', 'Planšetė', 'Pastatoma lempa', 'Vazonas', 'Baterijų pakrovėjas', 'Pagalvių komplektas', 'Oro drėkintuvas', 'Oro sausintuvas', 'Kavinukas elektrinis', 'Kilimas 2 X 3 metrų', 'Kilimėlis kojoms valyti', 'Užuolaidos', 'Vėjo malūnas', 'Dažai sienoms', 'Dažai luboms', 'Medinė dėžė'];
+// const produktuMasyvas = [{title: products[rand(0, 25)],
+//     price: decimalRand(1, 100),
+//     amount: rand(1, 20),
+//     total: function (){return this.price * this.amount},
+    
+// }];
+// console.log(produktuMasyvas);
+// console.log(produktuMasyvas[0].total());
 const produktuMasyvas = [];
 const produktuMasyvoIlgis = rand(1, 10);
 for (let i = 0; i < produktuMasyvoIlgis; i++) {
     produktuMasyvas.push({title: products[rand(0, 25)],
                         price: decimalRand(1, 100),
                         amount: rand(1, 20),
-                        total: function (){
-                            return this.price * this.amount;
-                        }})
+                        total: function (){return this.price * this.amount},  
+                    });
+        
 }
 
 console.log(products.length);
 const saskaituMasyvas = [];
 for (let i = 0; i < 100; i++) {
+    
     saskaituMasyvas.push({number: `INV${i + 1}`,
-                        products: 'a'});
+                        products: produktuMasyvas,
+                        total: function() {
+
+                        },
+                        vat: function(){return this.total * 0.21},
+                        grandTotal: function(){return this.total + this.vat}});
+    
 }
 
 console.log(saskaituMasyvas);
 console.log(saskaituMasyvas.length);
 console.log(produktuMasyvas);
+console.log(produktuMasyvas.length);
+console.log(produktuMasyvas[0].total());
+
+// viso produktu masyvo suma
+
+let produktuMasyvoSuma = 0;
+for (let i = 0; i < produktuMasyvas.length; i++) {
+    produktuMasyvoSuma += produktuMasyvas[i].total();
+}
+console.log(produktuMasyvoSuma);
+
 
 
