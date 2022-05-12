af10dc4c900e2dfb28dcb0d902ac5d84342e102b
Dalius Ram <ramonas.dalius@gmail.com>
2022-05-12 19:40:31 +0300

    update

--- 05-data/app.js
+++ 05-data/app.js
@@ -9,7 +9,7 @@ function rand(min, max) {
 function decimalRand(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.random() * (max - min + 1) + min;
+    return +(Math.random() * (max - min + 1) + min).toFixed(2);
 }
 
 const products = ['Dviratis', 'Triratis', 'Žvakidė', 'Stalas', 'Kėdė', 'Knygų lentyna', 'Paveikslas su rėmu', 'Veidrodis', 'Keturių vietų palapinė', 'Pripučiama valtis', 'Meškerė karosams gaudyti', 'Planšetė', 'Pastatoma lempa', 'Vazonas', 'Baterijų pakrovėjas', 'Pagalvių komplektas', 'Oro drėkintuvas', 'Oro sausintuvas', 'Kavinukas elektrinis', 'Kilimas 2 X 3 metrų', 'Kilimėlis kojoms valyti', 'Užuolaidos', 'Vėjo malūnas', 'Dažai sienoms', 'Dažai luboms', 'Medinė dėžė'];
@@ -17,7 +17,11 @@ const produktuMasyvas = [];
 const produktuMasyvoIlgis = rand(1, 10);
 for (let i = 0; i < produktuMasyvoIlgis; i++) {
     produktuMasyvas.push({title: products[rand(0, 25)],
-                        })
+                        price: decimalRand(1, 100),
+                        amount: rand(1, 20),
+                        total: function (){
+                            return this.price * this.amount;
+                        }})
 }
 
 console.log(products.length);
@@ -30,6 +34,5 @@ for (let i = 0; i < 100; i++) {
 console.log(saskaituMasyvas);
 console.log(saskaituMasyvas.length);
 console.log(produktuMasyvas);
-// console.log((decimalRand(1, 100)).round(2));
 
 
